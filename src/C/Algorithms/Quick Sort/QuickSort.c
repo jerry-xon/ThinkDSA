0522332c3da8401fad7507ca8531e5352a5b17a1
Jayesh Purswani <jppurswani2004@gmail.com>
2025-02-14 14:40:20 +0530

    updates

--- src/C/Algorithms/Quick Sort/QuickSort.c
+++ src/C/Algorithms/Quick Sort/QuickSort.c
@@ -30,8 +30,8 @@ void quickSort(int arr[], int low, int high)
     {
         int pi = partition(arr, low, high);
 
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        quickSort(arr, low, pi - 1);  //left subarray
+        quickSort(arr, pi + 1, high); //right subarray
     }
 }
 
